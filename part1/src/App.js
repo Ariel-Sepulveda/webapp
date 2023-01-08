f7dcf1d15ef163da4134322bd513f9394d024b6b
Ariel-Sepulveda <ariel.sepulveda.ar@gmail.com>
2023-01-08 13:58:10 -0300

    exercise 1.8

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,42 +1,23 @@
 import { useState } from 'react'
 
 const App = () => {
-  // save clicks of each button to its own state
+
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
-
-  const goodClick = () => {
-    setGood(good + 1)
-  }
-
-  const neutralClick = () => {
-    setNeutral(neutral + 1)
-  }
-
-  const badClick = () => {
-    setBad(bad + 1)
-  }
-
-  const totalVotes = () => (good + bad + neutral)
 
   return (
     <div>
       <h1>give feedback</h1>
 
-      <Button handler={goodClick} text="good" />
-      <Button handler={neutralClick} text="neutral" />
-      <Button handler={badClick} text="bad" />
+      <Button handler={() => setGood(good + 1)} text="good" />
+      <Button handler={() => setNeutral(neutral + 1)} text="neutral" />
+      <Button handler={() => setBad(bad + 1)} text="bad" />
 
       <h1>statistics</h1>
 
-      <Display text="good" votes={good} />
-      <Display text="neutral" votes={neutral} />
-      <Display text="bad" votes={bad} />
-      <Display text="all" votes={totalVotes()} />
-      <Display text="average" votes={totalVotes/3} />
-      <Display text="positive" votes={(good/(good + bad + neutral)) * 100} />
-      
+      <Statistics good={good} neutral={neutral} bad={bad} />
+
     </div>
   )
 }
@@ -52,6 +33,22 @@ const Display = ({ text, votes }) => {
     <div>
       {text + ' ' + votes}
     </div>
+  )
+}
+
+const Statistics = ({ good, neutral, bad }) => {
+
+  const totalVotes = () => (good + bad + neutral)
+
+  return (
+    <>  
+      <Display text="good" votes={good} />
+      <Display text="neutral" votes={neutral} />
+      <Display text="bad" votes={bad} />
+      <Display text="all" votes={totalVotes()} />
+      <Display text="average" votes={totalVotes() / 3} />
+      <Display text="positive" votes={(good / (good + bad + neutral)) * 100} />
+    </>
   )
 }
 
