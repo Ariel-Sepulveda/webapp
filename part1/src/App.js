63e0271a3e2ec7e6b5c6f4a658aac7e3686a625f
Ariel-Sepulveda <ariel.sepulveda.ar@gmail.com>
2023-01-08 15:04:41 -0300

    exercise 1.12

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,60 +1,31 @@
 import { useState } from 'react'
 
 const App = () => {
+  const anecdotes = [
+    'If it hurts, do it more often.',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.'
+  ]
 
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
+  const [selected, setSelected] = useState(0)
 
-  return (
-    <div>
-      <h1>give feedback</h1>
+  function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+  }
 
-      <Button handler={() => setGood(good + 1)} text="good" />
-      <Button handler={() => setNeutral(neutral + 1)} text="neutral" />
-      <Button handler={() => setBad(bad + 1)} text="bad" />
-
-      <h1>statistics</h1>
-
-      <Statistics good={good} neutral={neutral} bad={bad} />
-
-    </div>
-  )
-}
-
-const Button = ({ handler, text }) => <button onClick={handler}>{text}</button>
-
-
-const StatisticLine = ({ text, value }) => {
-  return (
-    <tr>
-      <td>{text}</td>
-      <td>{value}</td>
-    </tr>
-  )
-}
-
-const Statistics = ({ good, neutral, bad }) => {
-
-  const totalVotes = () => (good + bad + neutral)
-
-  if (totalVotes() === 0) {
-    return (
-      <div>no feedback given</div>
-    )
+  const randomAnecdote = () => {
+    setSelected(getRandomInt(anecdotes.length))
   }
 
   return (
-    <table>
-      <tbody>
-        <StatisticLine text="good" value={good} />
-        <StatisticLine text="neutral" value={neutral} />
-        <StatisticLine text="bad" value={bad} />
-        <StatisticLine text="all" value={totalVotes()} />
-        <StatisticLine text="average" value={(totalVotes() / 3).toFixed(1)} />
-        <StatisticLine text="positive" value={((good / totalVotes()) * 100).toFixed(2) + ' %'} />
-      </tbody>
-    </table>
+    <div>
+      <p>{anecdotes[selected]}</p>
+      <button onClick={randomAnecdote}>next anecdote</button>
+    </div>
   )
 }
 
